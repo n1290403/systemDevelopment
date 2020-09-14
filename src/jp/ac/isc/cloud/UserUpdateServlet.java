6dafad1ca0db3949d4923515315859a0bdf26f22
n1290403 <basara463143@gmail.com>
2020-09-14 09:41:45 +0900

    練習02_1

---
+++ src/jp/ac/isc/cloud/UserUpdateServlet.java
@@ -0,0 +1,54 @@
+package jp.ac.isc.cloud;
+
+import java.io.*;
+import java.sql.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+/**
+ * Servlet implementation class UserUpdateServlet
+ */
+@WebServlet("/UserUpdateServlet")
+public class UserUpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+				response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			Connection users = null;
+			try {
+			request.setCharacterEncoding("utf-8");
+			Class.forName("com.mysql.jdbc.Driver");
+			String name = null;
+			String id = null;
+			users = DriverManager.getConnection("UPDATE user_table SET name='" + name + "' WHERE id ='" + id + "'");
+			String id1 = request.getParameter("updateId");
+			String name1 = request.getParameter("updateName");
+			String picture = request.getParameter("updatePicture");
+			Statement state = users.createStatement();
+			state.executeUpdate("INSERT INTO user_table VALUE('" + id1 + "','" + name1 +
+			"','" + picture + "')");
+			state.close();
+			users.close();
+			response.sendRedirect("/select");
+			}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+			}
+			}catch(SQLException e){
+			e.printStackTrace();
+			}
+	}
+}
